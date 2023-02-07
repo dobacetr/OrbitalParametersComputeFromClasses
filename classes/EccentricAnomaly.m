5479a042ec3eb67a31c461ebded82f2763a1bed1
dobacetr <dogukan.benli@gmail.com>
2023-02-07 13:56:57 +0100

    Optimizations to EccentricAnomaly.ComputeFrom_Me_e
    Updated initial guess for continous Me inputs with 2nd order prediction on E for the initial guess.

--- classes/EccentricAnomaly.m
+++ classes/EccentricAnomaly.m
@@ -8,7 +8,7 @@ classdef EccentricAnomaly
     %% Elliptic Orbit Functions
     methods (Static)
         % Given MeanAnomaly(Me) and eccentricity(e)
-        function E = ComputeFrom_Me_e(Me, e)
+        function [E, residual] = ComputeFrom_Me_e(Me, e)
             assert(all(e<1), 'This function is valid for only elliptic orbits.');
             
             assert( ...
@@ -21,14 +21,18 @@ classdef EccentricAnomaly
             
             % Initialize results array
             E = zeros(1, sz);
+            residual = zeros(1, sz);
+            numIter = zeros(1, sz);
             
             % Indices for Me and e if they are vectors
             idxMe = 1;
             idxe  = 1;
             
+            Me = mod(Me, 2*pi);
+            
             % Solve for each point independently
             for iRes = 1 : sz
-                
+                %%
                 % Determine index of Me to be used
                 if length(Me) > 1
                     idxMe = iRes;
@@ -45,12 +49,54 @@ classdef EccentricAnomaly
                 fun = @(E) Me(idxMe) - E + e(idxe) * sin(E);
                 
                 % Initial Guess
-                Einit = Me(idxMe);
+                isMeContigous = false;
+                isMeLooped = false;
+                if iRes > 1
+                    MeDiff = Me(iRes) - Me(iRes-1);
+                    if(abs(MeDiff)<0.1)
+                       isMeContigous = true;
+                    elseif abs(MeDiff)>(2*pi-0.1) && abs(MeDiff)<(2*pi+0.1)
+                        isMeLooped = true;
+                    end
+                end
                 
-                [E(iRes), residual, errStatus] = SecondOrderNewtonRaphsonRoot(fun, Einit);
+                if (isMeLooped||isMeContigous) && iRes > 3
+%                     E = E0 + dE/didx*didx + 0.5*d2E/didx2*didx^2;
+                    E0 = E(iRes-2);
+                    Em1 = E(iRes-3);
+                    Ep1 = E(iRes-1);
+                    
+                    if Em1 > Ep1+0.9*2*pi
+                        Ep1 = Ep1 + 2*pi;
+                        isMeLooped = true;
+                    end
+                    if Em1 > E0+0.9*2*pi
+                        E0 = E0 + 2*pi;
+                        isMeLooped = true;
+                    end
+                    
+                    dEdidx = (Ep1-Em1)/2;
+                    d2Edidx2 = (Ep1+Em1-2*E0);
+                    Einit = E0 + dEdidx*2 + 0.5*d2Edidx2*4;
+                    if isMeLooped
+                        Einit = Einit - 2*pi;
+                    end
+                elseif isMeContigous && iRes > 1
+                    Einit = E(iRes-1);
+                else
+                    Einit = Me(idxMe);
+                end
+                
+                %%
+%                 optimOptions = optimset('fsolve');
+%                 optimOptions.Display = 'off';
+%                 optimOptions.TolX=1E-3;
+%                 [E(iRes), residual, errStatus] = fsolve(fun, Einit, optimOptions);
+%                 if errStatus < 0 && abs(residual) > 1E-3
+                [E(iRes), residual(iRes), errStatus, numIter(iRes)] = SecondOrderNewtonRaphsonRoot(fun, Einit);
 %                 [E(iRes), residual, errStatus] = NewtonRaphsonRoot(fun, Einit);
                 
-                if errStatus > 0 && abs(residual) > 1E-3
+                if errStatus > 0 && abs(residual(iRes)) > 1E-3
                     error('Error occurred while searching for Eccentric Anomaly.');
                 end
             end
